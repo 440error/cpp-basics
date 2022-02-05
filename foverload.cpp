d763fe3ee889ac1137a5d4919d9f26cac35269ef
Prakhar <rinayush@gmail.com>
2022-02-05 18:14:45 +0530

    comment-added

--- foverload.cpp
+++ foverload.cpp
@@ -13,11 +13,24 @@ int sum(int a ,int b,int c )
     return a+b+c ;
 }
 
+/*here as per seeing the code 
+the cout statement must be executed nd then the function call cout one statement
+ but in this first it is printing function call cout and the main cout */
 
+ // as per the code is the output should be
+ //the sum of 3 and 6 is using function with 2 arguments 9
+// while output is coming out 
+//  using function with 2 arguments the sum of 3 and 6 is 9
+
+
+// do comment down if u know why this problem is happening
+// since in my friends laptop code  is coming as stated is line 21
 int main(){
     
      cout << "the sum of 3 and 6 is : "<<sum(3,6) << endl ;
      cout << "the sum of 3,6 and 11 is : "<< sum(3,6,11) << endl ;
 
+
+    cout << " thank u " << endl ;
     return 0 ;
 }
