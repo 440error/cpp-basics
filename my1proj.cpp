4b73c940570cf2a8c3c469eebe64f3ed22a3732d
Prakhar <rinayush@gmail.com>
2022-02-07 00:44:06 +0530

    1-project

---
+++ my1proj.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+    cout << "hello world this is my first project " << endl;
+    cout << "working on vs code and learning cpp :) " << endl;
+
+    return 0 ;
+}
